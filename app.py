95d163b317aa1504482a8652408ac3a18f06e2ed
Nikhil-Koshti <nikhilkoshti2003@gmail.com>
2023-05-20 09:26:56 +0530

    Updated app.py to Host it publically

--- app.py
+++ app.py
@@ -165,5 +165,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.secret_key = "shfgdekh"
-    app.run(debug=True, port=5001)
+    #app.secret_key = "shfgdekh"
+    app.run(debug=False, port='0.0.0.0')
